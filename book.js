6f3ddcc63793022ec7c5ecf4a4c3d911748e2d71
Hrishikesh Makwana <hrishi1563@gmail.com>
2023-07-14 01:31:08 +0530

    First commit

---
+++ book.js
@@ -0,0 +1,12 @@
+
+
+
+
+        person = prompt("Enter Name"," ")
+        person1 = person
+       console.log(person)
+       let p=document.getElementById('name')
+       console.log(p)
+       p.textContent= person
+
+       
